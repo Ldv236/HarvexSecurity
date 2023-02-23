958800a4ca2828a8bf98b957c4a10a5e940429ae
Ldv236 <Ldv236@yandex.ru>
2023-02-24 00:21:26 +0200

    4. csrf on, use csrf-token

--- src/main/java/com/harvex/HarvexSecurity/services/RegistrationService.java
+++ src/main/java/com/harvex/HarvexSecurity/services/RegistrationService.java
@@ -3,6 +3,7 @@ package com.harvex.HarvexSecurity.services;
 import com.harvex.HarvexSecurity.models.Person;
 import com.harvex.HarvexSecurity.repositories.PeopleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -10,14 +11,21 @@ import javax.transaction.Transactional;
 @Service
 public class RegistrationService {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        /*
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
+        */
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
         peopleRepository.save(person);
     }
 }
